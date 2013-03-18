03b39297f77c2466f7e50dc2137012f8aa466a42
Melanie Rubia <melrub@gmail.com>
2013-03-18 18:02:16 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Faileo::Application.config.secret_token = '0ba6e362376283d2fbeb2f9d71ec990c063a2c96cc8eb483c9806e73aea8460cc3fafb0ca1cd229a8d8b4021f0b99f55d57a3862e7f3c5dab283dd5a4250f52e'
